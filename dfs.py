af5cc22231efe6037232f6ded7e85f4131f0354c
mohassan5286 <mohassan5286@gmail.com>
2024-10-28 20:05:57 +0300

    update GUI and DFS

--- dfs.py
+++ dfs.py
@@ -1,47 +1,46 @@
 import time
+
 from get_children import get_children
 from extract_path import extract_path
 
 def dfs(state):
-    goal_state = 12345678  # Goal configuration
+    goal_state = 12345678  
+
     if state == goal_state:
-        return [], 0, 0, 0, 0.0  # Return immediately if already solved
-
+        return [], 0, 0, 0, 0.0 
+    
     start = time.time()
-    stack = [[[state, "", 0]]]  # Initialize stack for LIFO behavior
-    visited = set()  # Track visited states to avoid cycles
-    max_depth_reached = 0
-    nodes_expanded = 0
 
-    while stack:
-        current_path = stack.pop()
+    stack     = [[[state, "", 0]]]  
+    visited   = set()  
+    forienter = set()  
+
+    while stack:  
+        current_path  = stack.pop()
         current_state = current_path[-1][0]
-        current_cost = current_path[-1][2]
+        current_cost  = current_path[-1][-1]
 
-        # Update the maximum depth reached
-        max_depth_reached = max(max_depth_reached, current_cost)
-        nodes_expanded += 1
-
-        # Mark the current state as visited
         visited.add(current_state)
 
-        # Generate possible moves (children)
         children_direction = get_children(current_state)
 
         for child, direction in children_direction:
-            if child == goal_state:  # Check if the goal state is found
+            if child == goal_state:
                 end = time.time()
+                
                 running_time = end - start
 
-                # Extract the path to the goal state
                 path = extract_path(current_path + [[child, direction, current_cost + 1]])
-                return path, current_cost + 1, nodes_expanded, max(max_depth_reached, current_cost + 1), running_time
+                nodes_expanded = len(visited)
+                
+                return path, current_cost + 1, nodes_expanded, current_cost + 1, running_time        
 
-            if child in visited:  # Skip if the child state has already been visited
+            if child in visited or child in forienter:
                 continue
             
-            # Add the new path to the stack for further exploration
-            new_path = current_path + [[child, direction, current_cost + 1]]
+            new_path = current_path + [[child, direction, current_cost + 1]] 
+            
+            forienter.add(child)
             stack.append(new_path)
-
+    
     return None
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,16 +1,1 @@
-import tracemalloc
-
-# Start tracking memory allocations
-tracemalloc.start()
-
-# Code to measure memory usage
-my_list = [1, 2, 3, 4, 5]
-
-# Get current and peak memory usage
-current, peak = tracemalloc.get_traced_memory()
-
-print(f"Current memory usage: {current / 1024:.2f} KB")
-print(f"Peak memory usage: {peak / 1024:.2f} KB")
-
-# Stop tracking memory
-tracemalloc.stop()
+# Create a Canvas for scrolling
